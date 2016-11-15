f1299b0a077347c3bf439503bf3281b24fdef977
shiki <shiki@NANAYA>
2016-11-15 08:27:20 +0800

    modified:   src/build.bat
       adding auto install part in build.bat

--- src/build.bat
+++ src/build.bat
@@ -50,7 +50,7 @@ ECHO.
 ::   3. make vim.exe
 ::
 :: ================================================
-DEL /S /Q %SRCDIR%\ObjGOHAMD64\
+DEL /S /Q %SRCDIR%\ObjCOHAMD64\
 DEL /F /Q vim.exe
 
 %VCDIR%\nmake -f Make_mvc.mak SDK_INCLUDE_DIR="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include" FEATURES=HUGE CPU=AMD64 GUI=no OLE=yes DIRECTX=yes PERL=C:\Perl64 DYNAMIC_PERL=yes PERL_VER=524 PYTHON=C:\Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=C:\Python35 DYNAMIC_PYTHON3=yes PYTHON3_VER=35  %1 IME=yes GIME=yes CSCOPE=yes USERNAME=nick USERDOMAIN=TSUKIHIME
@@ -64,6 +64,28 @@ ECHO.
 ECHO     [-] MAKE vim.exe FAILED! Please check makefile arguments.
 ECHO.
 )
+
+
+:: ================================================
+:: 
+::   4. install or reinstall vim8.0
+::   
+:: ================================================
+set VIMDIR=E:\programs\vim
+
+if exist %VIMDIR%\vim80\uninstal.exe (
+:: 
+
+        )
+del /f /q %VIMDIR%\_vimrc
+if exist %VIMDIR%\vim80 ( rmdir /s /q %VIMDIR%\vim80 )
+mkdir %VIMDIR%\vim80
+xcopy /s /q %SRCDIR%\..\runtime\*  %VIMDIR%\vim80
+xcopy %SRCDIR%\*.exe %VIMDIR%\vim80
+xcopy %SRCDIR%\GvimExt\gvimext.dll %VIMDIR%\vim80
+xcopy %SRCDIR%\xxd\xxd.exe %VIMDIR%\vim80
+
+
 
 :: vim: se ft=dosbash
 :: EOF
